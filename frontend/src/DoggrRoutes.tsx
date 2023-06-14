abc18405d3eac52b58b15df04cdcaa7e0e1c65ef
MrFiszbi1 <jaaf@pdx.edu>
2023-06-14 08:20:56 -0400

    Front End Code clean up

--- frontend/src/DoggrRoutes.tsx
+++ frontend/src/DoggrRoutes.tsx
@@ -2,7 +2,6 @@ import { CreateProfile } from "@/Components/CreateProfile.tsx";
 import { Home } from "@/Components/HomePage.tsx";
 import { Login } from "@/Components/Login.tsx";
 import { Logout } from "@/Components/Logout.tsx";
-import { Match } from "@/Components/Match.tsx";
 import { NavBar } from "@/Components/Navigation.tsx";
 import { ProfileProps } from "@/Components/Profile.tsx";
 import { ProtectedRoute } from "@/Components/ProtectedRoute.tsx";
@@ -27,7 +26,6 @@ export function DoggrRouter() {
 
 			<Routes>
 				<Route path="/" element={<Home />} />
-				<Route path="/match" element={<ProtectedRoute><Match /></ProtectedRoute>} />
 				<Route path="/create" element={<CreateProfile/>}/>
 				<Route path="/login" element={<Login />} />
 				<Route path="/logout" element={<Logout />} />
