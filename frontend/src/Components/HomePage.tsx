2a552e181457cd79ea5e2d33ef0daf1c3f282c0f
MrFiszbi <93094151+MrFiszbi@users.noreply.github.com>
2023-06-14 03:58:04 -0700

    Revert "home page gif bug fix"
    This reverts commit a79058916db73b5cd7cf10b020337a7d2f9decec.

--- frontend/src/Components/HomePage.tsx
+++ frontend/src/Components/HomePage.tsx
@@ -1,16 +1,18 @@
 import { UsersList } from "@/Components/UsersList.tsx";
 
 export const Home = () => {
+	const gif = "../src/assets/gifs/Dr_Evil_Welcome.gif";
 	return (
 		<div className="mx-auto mb-3">
 			<h1 className="text-4xl font-bold mb-3 text-center">Welcome To The Gifiks Website</h1>
 			<h2 className="text-2xl font-bold mb-8 text-center">Share your favorite gifs with the world</h2>
 			<div
+				key={gif}
 				className="bg-primary flex flex-col items-center rounded border w-4/5 mx-auto pt-5"
 			>
 				<img
 					className="rounded"
-					src={"./Dr_Evil_Welcome.gif"}
+					src={gif}
 					alt="Welcome Gif"
 					style={{ width: "960px", height: "554px" }}
 				/>
