1bc7993cf200b22c1400447a74d1bcfb7ded548b
MrFiszbi1 <jaaf@pdx.edu>
2023-06-14 09:23:14 -0400

    backend clean up

--- backend/src/db/entities/User.ts
+++ backend/src/db/entities/User.ts
@@ -1,12 +1,9 @@
 import { Entity, Property, Unique, OneToMany, Collection, Cascade } from "@mikro-orm/core";
 import { SoftDeletable } from "mikro-orm-soft-delete";
 import { DoggrBaseEntity } from "./DoggrBaseEntity.js";
-import { Match } from "./Match.js";
 
 import { Enum } from "@mikro-orm/core";
-import { Message } from "./Message.js";
 import { Gifs } from "./Gifs.js";
-import { Pass } from "./Pass.js";
 
 export enum UserRole {
 	ADMIN = 'Admin',
@@ -33,50 +30,6 @@ export class User extends DoggrBaseEntity {
 
 	@Property({fieldName: 'gif_uri'})
 	gifUri!: string;
-
-	// Note that these DO NOT EXIST in the database itself!
-	@OneToMany(
-		() => Match,
-		match => match.owner,
-		{cascade: [Cascade.PERSIST, Cascade.REMOVE]}
-	)
-	matches!: Collection<Match>;
-
-	@OneToMany(
-		() => Match,
-		match => match.matchee,
-		{cascade: [Cascade.PERSIST, Cascade.REMOVE]}
-	)
-	matched_by!: Collection<Match>;
-
-	@OneToMany(
-		() => Pass,
-		pass => pass.owner,
-		{cascade: [Cascade.PERSIST, Cascade.REMOVE]}
-	)
-	passes!: Collection<Pass>;
-
-	@OneToMany(
-		() => Match,
-		pass => pass.matchee,
-		{cascade: [Cascade.PERSIST, Cascade.REMOVE]}
-	)
-	passed_by!: Collection<Pass>;
-
-	// Orphan removal used in our Delete All Sent Messages route to single-step remove via Collection
-	@OneToMany(
-		() => Message,
-		message => message.sender,
-		{cascade: [Cascade.PERSIST, Cascade.REMOVE], orphanRemoval: true}
-	)
-	messages_sent!: Collection<Message>;
-
-	@OneToMany(
-		() => Message,
-		message => message.receiver,
-		{cascade: [Cascade.PERSIST, Cascade.REMOVE], orphanRemoval: true}
-	)
-	messages_received!: Collection<Message>;
 
 	@OneToMany(
 		() => Gifs,
